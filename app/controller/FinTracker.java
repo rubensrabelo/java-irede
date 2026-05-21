40439accb452476a1637a4eef24fab9ace2d6120
Rubens Rabelo <rubensrabelo123@gmail.com>
2026-05-21 12:18:13 -0300

    feat(controller): start controller

--- app/controller/FinTracker.java
+++ app/controller/FinTracker.java
@@ -1,5 +1,24 @@
 package app.controller;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import app.model.Transaction;
+
 public class FinTracker {
-    
+    public boolean addTransaction() {
+        return false;
+    }
+
+    public List<Transaction> listTransactions() {
+        return null;
+    }
+
+    public boolean removeTransaction() {
+        return false;
+    }
+
+    public BigDecimal calculateTotalBalance() {
+        return null;
+    }
 }
